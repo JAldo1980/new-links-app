0a2a98247db895cec71b21d8601e237c2cb95d83
James Alderman <jamesalderman1980@gmail.com>
2025-08-19 07:36:49 +0100

    updated internal link styling and main image

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,44 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div></div>
+      <div classs="wrapper">
+        <div class="container">
+          <header>
+            <img
+              src="/public/jene-knight.webp"
+              alt="Jene Knight"
+              class="main-img"
+            />
+            <h1>Jene Knight</h1>
+            <p>Head of Digital | Extramile Digital</p>
+          </header>
+          <section class="social-section">
+            <div>
+              <a href="#" class="hyper-link-el">
+                LinkedIn
+              </a>
+            </div>
+            <div>
+              <a href="#" class="hyper-link-el">
+                Instagram
+              </a>
+            </div>
+            <div>
+              <a href="#" class="hyper-link-el">
+                Website
+              </a>
+            </div>
+            <div>
+              <a href="#" class="hyper-link-el">
+                Email
+              </a>
+            </div>
+          </section>
+          <main>
+            <div class=""></div>
+          </main>
+        </div>
+      </div>
     </>
   );
 }
