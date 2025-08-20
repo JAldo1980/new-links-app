52c5d72a4afa26812391d6fa31238fa30f5bc4e1
James Alderman <jamesalderman1980@gmail.com>
2025-08-20 20:33:08 +0100

    Update LinkData

--- src/data/LinkData.jsx
+++ src/data/LinkData.jsx
@@ -3,26 +3,27 @@ import React from "react";
 const LinkData = [
   {
     image: "/images/...",
-    name: "LinkedIn",
-    link: "#",
-    alt: "LinkedIn logo",
+    name: "We Are Hiring!",
+    link: "https://www.extramiledigital.com/careers/",
+    alt: "Extramile logo",
   },
   {
     image: "/images/...",
-    name: "Instagram",
-    link: "#",
+    name: "Our Services",
+    link: "https://www.extramiledigital.com/services/",
     alt: "Instagram logo",
   },
   {
-    image: "/images/...",
+    image:
+      "https://www.extramiledigital.com/wp-content/themes/extramile-digital/assets/img/Extramile-Digital-Logo-2x.png",
     name: "Website",
-    link: "#",
+    link: "https://www.extramiledigital.com/",
     alt: "Website logo",
   },
   {
     image: "/images/...",
-    name: "email",
-    link: "#",
+    name: "Contact me",
+    link: "https://www.extramiledigital.com/contact/",
     alt: "Email icon",
   },
 ];
